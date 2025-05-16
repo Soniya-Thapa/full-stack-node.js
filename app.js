f3135f0ceff000dfb291c33346a958162865bb52
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-05-16 20:40:44 +0545

    cors

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-
+const cors = require("cors")
 const bookRoute = require("./routes/book.route")
 
 require("./database/connection") //./ = ma jaha xuuu, database = tyeha yo folder xa , / = tyo bhitra , connection = yo file xa 
+
+app.use(cors({
+  origin : "http://localhost:5173"
+}))
+
 app.use(express.json()) // yo code hanexi json data expressley bujhxa and postman bata post gareko json data dekhinxa 
 
 app.use("",bookRoute)
