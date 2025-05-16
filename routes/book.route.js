64d13e1415c5d0d434da8f3f593184de2b6f0e2b
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-05-16 19:42:26 +0545

    route

--- routes/book.route.js
+++ routes/book.route.js
@@ -7,4 +7,6 @@ router.route("/books").get(fetchBooks).post(addBook)
 router.route("/books/:id").delete(deleteBook).patch(editBook).get(singleFetchBook)
 
 module.exports = router
-// module.exports = {router} //{}=> taba matra halney jaba multiple lai export garna parxa  ra export garda direct exports.router = garera garna mildaina cause yeha hamley router ma aaru dherai kura thapera export garna parxa. yedi hami exports.router = yo garxam bhani hamro router ma append gareko kura export hudaina
+// module.exports = {router} //{}=> taba matra halney jaba multiple lai export garna parxa  ra export garda direct exports.router 
+// = garera garna mildaina cause yeha hamley router ma aaru dherai kura thapera export garna parxa. yedi hami exports.router 
+// = yo garxam bhani hamro router ma append gareko kura export hudaina
